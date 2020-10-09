4d3546f1fbe0e3658177c46e20579d098b0fa578
alana170 <52054877+alana170@users.noreply.github.com>
2020-10-09 16:59:23 -0400

    Called Weather API by zipcode

--- app/app.py
+++ app/app.py
@@ -1,14 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
 import requests, json
-
+import datetime
 
 app = Flask(__name__)
 
 
-@app.route("/", methods= ["GET", "POST"])
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
 
+@app.route("/temperature", methods= ["POST", "GET"])
+def temperature():
+    
+    zipcode = request.form['zip']
+    res = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+'&units=imperial&appid=d6c3883364ca9dae46be60aa932ad58b')
+    json_object = res.json()
+
+   
+    temp_f = float(json_object['main']['temp'])
+    mintemp = float(json_object['main']['temp_min'])
+    maxtemp = float(json_object['main']['temp_max'])
+  
+    mintemp = round(mintemp, 2)
+    maxtemp = round(maxtemp, 2)
+    temp_f = round(temp_f, 2)
+
+    description = json_object['weather'][0]['description']
+    humidity = json_object['main']['humidity']
+    cityName = json_object['name']
+    ws = json_object['wind']['speed']
+    sunrise = json_object['sys']['sunrise']
+    sunset = json_object['sys']['sunset']
+    sunrise = datetime.datetime.fromtimestamp(sunrise).strftime('%H:%M')
+    sunset = datetime.datetime.fromtimestamp(sunset).strftime('%H:%M')
+
+    obj = {
+        "temp": temp_f,
+        "zipcode": zipcode,
+        "maxtemp": maxtemp, 
+        "mintemp":mintemp, 
+        "descr":description, 
+        "hum": humidity,
+        "cityName": cityName,
+        "ws" : ws,
+        "rise" : sunrise,
+        "set" : sunset,
+    }
+
+    return render_template("temp.html", obj = obj)
 
 
 if __name__=='__main__' :
