a63ce1f7b9f2be4a520ad5232161f69a9c91f7a4
alana170 <52054877+alana170@users.noreply.github.com>
2020-10-30 16:59:49 -0400

    started on 5 day forecast and outline

--- app/app.py
+++ app/app.py
@@ -54,5 +54,64 @@ def temperature():
     return render_template("temp.html", obj = obj, message = msg)
 
 
+@app.route("/5day3hour", methods= ["POST", "GET"])
+def weeklyTemperature():
+    msg = ""
+    zipcode = request.form['zip']
+    res = requests.get('http://api.openweathermap.org/data/2.5/forecast?zip='+zipcode+'&units=imperial&appid=d6c3883364ca9dae46be60aa932ad58b')
+    json_object = res.json()
+
+    temp = {}
+    for i in json_object['list']:
+        temp[i['dt_txt']] = i['main']['temp']
+        dn = i['sys']['pod']
+
+    obj = {
+        "dttemp": temp,
+        "dayornight": dn
+    }
+
+    """ try :
+        temp_f = float(json_object['main']['temp'])
+        mintemp = float(json_object['main']['temp_min'])
+        maxtemp = float(json_object['main']['temp_max'])
+            
+        mintemp = round(mintemp, 2)
+        maxtemp = round(maxtemp, 2)
+        temp_f = round(temp_f, 2)
+
+        description = json_object['weather'][0]['description']
+        humidity = json_object['main']['humidity']
+        cityName = json_object['name']
+        ws = json_object['wind']['speed']
+        sunrise = json_object['sys']['sunrise']
+        sunset = json_object['sys']['sunset']
+        sunrise = datetime.datetime.fromtimestamp(sunrise).strftime('%H:%M')
+        sunset = datetime.datetime.fromtimestamp(sunset).strftime('%H:%M')
+
+        obj = {
+            "temp": temp_f,
+            "zipcode": zipcode,
+            "maxtemp": maxtemp, 
+            "mintemp":mintemp, 
+            "descr":description, 
+            "hum": humidity,
+            "cityName": cityName,
+            "ws" : ws,
+            "rise" : sunrise,
+            "set" : sunset,
+        }
+
+    except(KeyError):
+        obj = {""}
+        msg = "No weather info for " + str(zipcode)
+"""
+  
+    return render_template("weektemp.html")
+
+
+
+
+
 if __name__=='__main__' :
     app.run(debug=True)
