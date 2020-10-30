a6ca64317f7417fe6eb9ba619ebbf10df17d523b
alana170 <52054877+alana170@users.noreply.github.com>
2020-10-30 10:57:17 -0400

    catches error from incorrect zipcode

--- app/app.py
+++ app/app.py
@@ -5,49 +5,53 @@ import datetime
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def index():
     return render_template('form.html')
 
 @app.route("/temperature", methods= ["POST", "GET"])
 def temperature():
-    
+    msg = ""
     zipcode = request.form['zip']
     res = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+'&units=imperial&appid=d6c3883364ca9dae46be60aa932ad58b')
     json_object = res.json()
+    try :
+        temp_f = float(json_object['main']['temp'])
+        mintemp = float(json_object['main']['temp_min'])
+        maxtemp = float(json_object['main']['temp_max'])
+            
+        mintemp = round(mintemp, 2)
+        maxtemp = round(maxtemp, 2)
+        temp_f = round(temp_f, 2)
 
-   
-    temp_f = float(json_object['main']['temp'])
-    mintemp = float(json_object['main']['temp_min'])
-    maxtemp = float(json_object['main']['temp_max'])
+        description = json_object['weather'][0]['description']
+        humidity = json_object['main']['humidity']
+        cityName = json_object['name']
+        ws = json_object['wind']['speed']
+        sunrise = json_object['sys']['sunrise']
+        sunset = json_object['sys']['sunset']
+        sunrise = datetime.datetime.fromtimestamp(sunrise).strftime('%H:%M')
+        sunset = datetime.datetime.fromtimestamp(sunset).strftime('%H:%M')
+
+        obj = {
+            "temp": temp_f,
+            "zipcode": zipcode,
+            "maxtemp": maxtemp, 
+            "mintemp":mintemp, 
+            "descr":description, 
+            "hum": humidity,
+            "cityName": cityName,
+            "ws" : ws,
+            "rise" : sunrise,
+            "set" : sunset,
+        }
+
+    except(KeyError):
+        obj = {""}
+        msg = "No weather info for " + str(zipcode)
+
   
-    mintemp = round(mintemp, 2)
-    maxtemp = round(maxtemp, 2)
-    temp_f = round(temp_f, 2)
-
-    description = json_object['weather'][0]['description']
-    humidity = json_object['main']['humidity']
-    cityName = json_object['name']
-    ws = json_object['wind']['speed']
-    sunrise = json_object['sys']['sunrise']
-    sunset = json_object['sys']['sunset']
-    sunrise = datetime.datetime.fromtimestamp(sunrise).strftime('%H:%M')
-    sunset = datetime.datetime.fromtimestamp(sunset).strftime('%H:%M')
-
-    obj = {
-        "temp": temp_f,
-        "zipcode": zipcode,
-        "maxtemp": maxtemp, 
-        "mintemp":mintemp, 
-        "descr":description, 
-        "hum": humidity,
-        "cityName": cityName,
-        "ws" : ws,
-        "rise" : sunrise,
-        "set" : sunset,
-    }
-
-    return render_template("temp.html", obj = obj)
+    return render_template("temp.html", obj = obj, message = msg)
 
 
 if __name__=='__main__' :
